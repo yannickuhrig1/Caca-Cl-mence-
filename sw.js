3425d023222135b5a9ee47c23288035afe496eb3
yannickuhrig1 <yannick.uhrig@gmail.com>
2026-02-17 19:38:42 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'caca-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
